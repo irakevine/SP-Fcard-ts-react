c89e8367c2f4ee434ce3f484aabec05d1c1dd25f
irakevine <ikevine2016@gmail.com>
2023-07-18 18:17:50 +0200

    adding ui

---
+++ src/API.ts
@@ -0,0 +1,15 @@
+
+
+
+
+export enum Difficulty{
+    EASY= "easy",
+    MEDIUM= "medium",
+    HARD="hard"
+}
+export const fetchQuizQuestions = async(amount:number,difficulty:Difficulty)=>{
+const endppoint= `https//opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+const data=  await (await fetch(endppoint)).json()
+console.log(data);
+
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+// import QuestionCard from './component/QuestionCard';
 
+import { fetchQuizQuestions } from './API';
+import { Difficulty } from './API';
+const TOTATAL_QUESTION= 10
+const App = () => {
+  const [loading,setLoading] = useState(false)
+  const[questions, setQuestions ] =useState([])
+  const [ number , setNumber] = useState(0)
+  const [ userAnswers , setUserAnswers] = useState([])
+  const [ score, setScore] = useState(0)
+  const [ gameOver, setGameOver] = useState(true)
+console.log(fetchQuizQuestions(TOTATAL_QUESTION, Difficulty.EASY));
 
-function App() {
+  const startTrivia = async ()=>{}
+  const checkAnswer= (e: React.MouseEvent<HTMLButtonElement>)=>{}
+  const nextQuestion=()=>{
+
+  }
   return (
     <div className="App">
-      Quiz
+      <h1>React Quiz</h1>
+      <button className='start' onClick={startTrivia}>
+        start
+      </button>
+      <p className='score'> score:</p>
+      <p>Loading Questions ...</p>
+      {/* <QuestionCard  
+       questionNumber={number+1}
+       totalQuestion={TOTATAL_QUESTION}
+       question={questions[number].question}
+       answers={questions[number].answers}
+       userAnswer= {userAnswers ? userAnswers[number]: undefined}
+       callback={checkAnswer}
+      /> */}
+      <button className='next' onClick={nextQuestion}> Next Question</button>
+
     </div>
   );
 }
