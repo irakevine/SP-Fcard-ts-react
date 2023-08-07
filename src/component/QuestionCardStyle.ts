5284f1c2b1aa6ba85fc2905110a98df19a017f27
irakevine <ikevine2016@gmail.com>
2023-08-07 23:49:11 +0200

    modifying

--- src/component/QuestionCardStyle.ts
+++ src/component/QuestionCardStyle.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
   max-width: 1100px;
@@ -32,10 +32,10 @@ export const ButtonWrapper = styled.div<ButtonWrapperProps>`
     margin: 5px 0;
     background: ${({ correct, userClicked }) =>
       correct
-        ? 'linear-gradient(90deg, #56FFA4, #59BC86)'
+        ? "linear-gradient(90deg, #56FFA4, #59BC86)"
         : !correct && userClicked
-        ? 'linear-gradient(90deg, #FF5656, #C16868)'
-        : 'linear-gradient(90deg, #56ccff, #6eafb4)'};
+        ? "linear-gradient(90deg, #FF5656, #C16868)"
+        : "linear-gradient(90deg, #56ccff, #6eafb4)"};
     border: 3px solid #ffffff;
     box-shadow: 1px 2px 0px rgba(0, 0, 0, 0.1);
     border-radius: 10px;
